dd13be314c0898702c59fd3bd77d79ddd32a8c8c
Simon Gorin <github.com.darkness667@passmail.net>
2026-09-07 17:36:00 +0200

    Remove calls `skip_on_cran()`, skipping when no token is in the env var is enough

--- tests/testthat/test-oag_fetch.R
+++ tests/testthat/test-oag_fetch.R
@@ -726,8 +726,6 @@ test_that("oag_fetch() is successful when query is correctly formatted", {
   expect_error(oag_fetch("invalid_string"))
   skip_if_not(nzchar(Sys.getenv("oag_api_refresh_token")))
 
-  skip_on_cran()
-
   expect_error(
     oag_fetch(
       "research-products",
@@ -818,7 +816,6 @@ test_that("oag_fetch() is successful when query is correctly formatted", {
 ## Success ----
 
 test_that("oag_fetch() is successful when query is correctly formatted", {
-  skip_on_cran()
   skip_if_not(nzchar(Sys.getenv("oag_api_refresh_token")))
 
   options <- oag_options(
