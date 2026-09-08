7148b0a7674ab6be6c113e00ea91634a00b4cca5
Simon Gorin <github.com.darkness667@passmail.net>
2026-09-08 14:33:01 +0200

    Add unit tests for the organizations parsing function

--- tests/testthat/test-oag_parser.R
+++ tests/testthat/test-oag_parser.R
@@ -1,4 +1,6 @@
-test_that("Parsing functions work", {
+test_that("Parsing functions work (research products", {
+  skip_if_not(nzchar(Sys.getenv("oag_api_refresh_token")))
+
   day_from <- sample(1:22, 1)
   day_to <- day_from + 6
   day_from <- as.character(ifelse(
@@ -11,40 +13,48 @@ test_that("Parsing functions work", {
   month <- as.character(ifelse(month < 10, paste0("0", month), month))
   year <- as.character(sample(2023:2025, 1))
 
-  snsf_pub <- oag_fetch(
-    "research-products",
-    type = "publication",
-    relProjectFundingShortName = "SNSF",
-    fromPublicationDate = paste0(year, "-", month, "-", day_from),
-    toPublicationDate = paste0(year, "-", month, "-", day_to),
-    options = oag_options(pageSize = 100, cursor = TRUE)
+  snsf_pub <- suppressMessages(
+    oag_fetch(
+      "research-products",
+      type = "publication",
+      relProjectFundingShortName = "SNSF",
+      fromPublicationDate = paste0(year, "-", month, "-", day_from),
+      toPublicationDate = paste0(year, "-", month, "-", day_to),
+      options = oag_options(pageSize = 100, cursor = TRUE)
+    )
   )
 
-  snsf_ds <- oag_fetch(
-    "research-products",
-    type = "dataset",
-    relProjectFundingShortName = "SNSF",
-    fromPublicationDate = paste0(year, "-", month, "-", day_from),
-    toPublicationDate = paste0(year, "-", month, "-", day_to),
-    options = oag_options(pageSize = 100, cursor = TRUE)
+  snsf_ds <- suppressMessages(
+    oag_fetch(
+      "research-products",
+      type = "dataset",
+      relProjectFundingShortName = "SNSF",
+      fromPublicationDate = paste0(year, "-", month, "-", day_from),
+      toPublicationDate = paste0(year, "-", month, "-", day_to),
+      options = oag_options(pageSize = 100, cursor = TRUE)
+    )
   )
 
-  snsf_soft <- oag_fetch(
-    "research-products",
-    type = "software",
-    relProjectFundingShortName = "SNSF",
-    fromPublicationDate = paste0(year, "-", month, "-", day_from),
-    toPublicationDate = paste0(year, "-", month, "-", day_to),
-    options = oag_options(pageSize = 100, cursor = TRUE)
+  snsf_soft <- suppressMessages(
+    oag_fetch(
+      "research-products",
+      type = "software",
+      relProjectFundingShortName = "SNSF",
+      fromPublicationDate = paste0(year, "-", month, "-", day_from),
+      toPublicationDate = paste0(year, "-", month, "-", day_to),
+      options = oag_options(pageSize = 100, cursor = TRUE)
+    )
   )
 
-  snsf_other <- oag_fetch(
-    "research-products",
-    type = "other",
-    relProjectFundingShortName = "SNSF",
-    fromPublicationDate = paste0(year, "-", month, "-", day_from),
-    toPublicationDate = paste0(year, "-", month, "-", day_to),
-    options = oag_options(pageSize = 100, cursor = TRUE)
+  snsf_other <- suppressMessages(
+    oag_fetch(
+      "research-products",
+      type = "other",
+      relProjectFundingShortName = "SNSF",
+      fromPublicationDate = paste0(year, "-", month, "-", day_from),
+      toPublicationDate = paste0(year, "-", month, "-", day_to),
+      options = oag_options(pageSize = 100, cursor = TRUE)
+    )
   )
 
   expect_no_error(
@@ -74,4 +84,26 @@ test_that("Parsing functions work", {
     )
   )
   expect_equal(length(snsf_other), nrow(parsed_other))
+})
+
+test_that("Parsing functions work (organizations)", {
+  skip_if_not(nzchar(Sys.getenv("oag_api_refresh_token")))
+
+  res_orgs <- suppressMessages(
+    oag_fetch(
+      "organizations",
+      countryCode = "BE",
+      options = oag_options(pageSize = 100, cursor = TRUE)
+    )
+  )
+
+  samp <- sample(seq_along(res_orgs), 100)
+
+  expect_no_error(
+    parsed_orgs <- suppressMessages(
+      parse_entity_organizations(res_orgs[samp])
+    )
+  )
+
+  expect_equal(length(res_orgs[samp]), nrow(parsed_orgs))
 })
